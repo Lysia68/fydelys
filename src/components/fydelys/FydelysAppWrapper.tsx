95b9db1cd8ef0b5903bc3e20933da00aa9424400
Lysia68 <163148028+Lysia68@users.noreply.github.com>
2026-03-12 15:21:05 +0100

    ss

--- src/components/fydelys/FydelysAppWrapper.tsx
+++ src/components/fydelys/FydelysAppWrapper.tsx
@@ -1,8 +1,2 @@
 "use client";
-import React from "react";
-
-// FydelysApp.jsx n'a pas de déclaration TypeScript — on passe par require
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const App = require("./FydelysApp").default as React.ComponentType<any>;
-
-export default App;
+export { default } from "./FydelysApp";
