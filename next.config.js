dba777b441748d18059b6ccc6d33f4a3859153c3
Lysia68 <163148028+Lysia68@users.noreply.github.com>
2026-03-20 14:59:33 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const buildDate = new Date().toLocaleDateString("fr-FR")
 const nextConfig = {
   typescript: {
     // Ignore TypeScript errors in JSX files (inline styles boxSizing etc.)
     ignoreBuildErrors: true,
+  },
+  env: {
+    NEXT_PUBLIC_BUILD_DATE: buildDate,
   },
 }
 module.exports = nextConfig
