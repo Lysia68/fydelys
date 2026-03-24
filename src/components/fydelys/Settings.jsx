0de46de2362aa94fd6d406e076a3b7c755eb58bc
Lysia68 <163148028+Lysia68@users.noreply.github.com>
2026-03-24 09:45:05 +0100

    fix div

--- src/components/fydelys/Settings.jsx
+++ src/components/fydelys/Settings.jsx
@@ -945,6 +945,7 @@ function Settings({ isMobile, onImpersonate }) {
                     )
                   )}
                 </div>
+                </div>
               </div>
             </div>
           ))}
